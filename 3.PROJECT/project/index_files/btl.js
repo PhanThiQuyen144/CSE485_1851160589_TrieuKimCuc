5cd3f0a35e0ea4d4e6b31818fcd295cb8ea28968
KimCuc273 <you@example.com>
2020-11-01 16:56:15 +0700

    add project

---
+++ 3.PROJECT/project/index_files/btl.js
@@ -0,0 +1,27 @@
+// $(document).ready(function(){
+//     $(".menu li").hover(function(){
+//         $(this).find("ul:first").slideDown(500);
+//     },function(){
+//         $(this).find("ul:first").hide(300);
+
+//     });
+// });
+// $(document).ready(function () {
+//     $(".menu-bar").click(function () { 
+//         $(".form-inline").toggleClass("search-bar");
+//     });
+// });
+// $(document).ready(function () {
+//     $(".icon").click(function(){
+//         $(".list-nav").slideToggle();
+//         $(".list-sub ").css("display","none");
+//     });
+// });
+
+$(document).ready(function () {
+    $("#respon-bar").click(function(){
+        $(".navbar").slideToggle();
+        $(".dropdown-menu a ").css("display","none");
+    });
+    $()
+});
